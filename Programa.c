4c6c983ca70d34ec321c3977e60a5682d9f088fc
Anderson Filho <116195013+Andersonpsf@users.noreply.github.com>
2025-11-14 20:55:05 -0300

    Função de inserir_clientes adicionada e de listar_clientes

--- Programa.c
+++ Programa.c
@@ -4,13 +4,13 @@
 #define SENHA 123
 
 
-typedef struct Cliente{
-	char nome;
-	char telefone;
-	char empresa;
-	char email;
-	char cep;
-	char celular;
+typedef struct{
+	char nome[100];
+	char telefone[15];
+	char empresa[50];
+	char email[100];
+	char cep[15];
+	char celular[15];
 }cliente;
 
 
@@ -21,9 +21,38 @@ typedef struct Data{
 }data;
 
 
-
-void inserir_clientes()
+/*
+* Função: inserir_clientes
+* Descrição: Insere os dados de um cliente na lista.
+* Parâmetros:
+* listaClientes: vetor de clientes.
+* cliente: estrutura contendo os dados do cliente.
+* quantClientes: Números de Clientes Cadastrados
+* Retorna:
+* A Nova Quantidade de Clientes
+Cadastrados
+*/
+int inserir_clientes(cliente listaClientes[], int quantClientes)
 {
+	printf("\nInsira as informações do Cliente: \n");
+	printf("\nNome:");
+	scanf(" %s", listaClientes[quantClientes].nome);
+	
+	printf("\nTelefone:");
+	scanf(" %s", listaClientes[quantClientes].telefone);
+
+	printf("\nEmpresa:");
+	scanf(" %s", listaClientes[quantClientes].empresa);
+	
+	printf("\nEmail:");
+	scanf(" %s", listaClientes[quantClientes].email);
+	
+	printf("\nCEP:");
+	scanf(" %s", listaClientes[quantClientes].cep);
+	
+	printf("\nCelular:");
+	scanf(" %s", listaClientes[quantClientes].celular);
+	return quantClientes+1;
 	
 }
 
@@ -37,9 +66,36 @@ void excluir_cliente()
 	
 }
 
-void listagem_nome()
+/*
+* Função: listar_nomes
+* Descrição: Lista o .
+* Parâmetros:
+* lista: vetor de clientes.
+* cliente: estrutura contendo os dados do cliente.
+* Retorna:
+* 1 se a inserção for bem-sucedida, 0 caso
+contrário.
+*/
+
+void listar_nomes(cliente listaClientes[], int quantClientes)
 {
+
+	int i;
+	for(i=0; i<quantClientes; i++)
+	{
+		printf("\nCliente %d:", i+1);
+		printf("\nNome: %s", listaClientes[i].nome);
+		
+		printf("\nTelefone: %s", listaClientes[i].telefone);
 	
+		printf("\nEmpresa: %s", listaClientes[i].empresa);
+		
+		printf("\nEmail: %s", listaClientes[i].email);
+		
+		printf("\nCEP: %s", listaClientes[i].cep);
+		
+		printf("\nCelular: %s\n", listaClientes[i].celular);
+	}
 }
 
 void pesquisar_nome_empresa()
@@ -54,6 +110,8 @@ void pesquisar_nome_cliente()
 
 int main()
 {
+    cliente listaClientes[MAX_CLIENTES];
+	int quantClientes = 0;
 	int opc;
 	do{
 		printf("\n=====MENU=====\n");
@@ -69,10 +127,10 @@ int main()
 		
 		switch(opc)
 		{
-			case 1: inserir_clientes(); break;
+			case 1: quantClientes = inserir_clientes(listaClientes, quantClientes); break;
 			case 2: alterar_dados(); break;
 			case 3: excluir_cliente(); break;
-			case 4: listagem_nome(); break;
+			case 4: listar_nomes(listaClientes, quantClientes); break;
 			case 5: pesquisar_nome_empresa(); break;
 			case 6: pesquisar_nome_cliente(); break;
 			default: printf("\nOpcao Invalida!!\n");
